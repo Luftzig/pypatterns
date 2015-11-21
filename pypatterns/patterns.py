9f6a9fe9497d927117a59a35c40d6d2ff9280461
Yoav Luft <yoav.luft@ajillionmax.com>
2015-11-21 14:19:25 +0200

    Supporting guard decorator with: literals, predicate functions, Any(*types), Any.has(*attribute, **attribute_and_predicate)

--- pypatterns/patterns.py
+++ pypatterns/patterns.py
@@ -1,4 +1,5 @@
-from functools import wraps
+from functools import wraps, partial
+import re
 
 
 class NoMatch(Exception):
@@ -7,14 +8,73 @@ class NoMatch(Exception):
 
 class Any(object):
 
-    def __init__(self, *types):
+    def __init__(self, *allowed_types):
+        self._allowed_types = allowed_types
+
+    def __call__(self, arg):
+        if hasattr(self, 'matcher'):
+            return self.matcher(arg)
+        return issubclass(type(arg), tuple(self._allowed_types))
+
+    @staticmethod
+    def of(*matches):
+        pass
+
+    @staticmethod
+    def has(*attrs, **attr_with_check):
+        matcher = Any()
+
+        def has_matcher(arg):
+            def has_attr_and_predicate(key_predicate):
+                key, predicate = key_predicate
+                return hasattr(arg, key) and _bake_predicate(predicate)(getattr(arg, key))
+
+            return any(map(partial(hasattr, arg), attrs)) or any(
+                map(has_attr_and_predicate, attr_with_check.items()))
+
+        matcher.matcher = has_matcher
+        return matcher
+
+    @staticmethod
+    def re(str_or_re):
         pass
 
 
+def _bake_predicate(p):
+    if callable(p):
+        return p
+    elif isinstance(p, Any):
+        return p
+    else:
+        return lambda a: a == p
+
+
+def _bake_predicates(predicates):
+    return [_bake_predicate(p) for p in predicates]
+
+
+def _bake_kw_predicates(kw_predicates):
+    return {k: p for k, p in kw_predicates if callable(p)}
+
+
 def guard(*predicates, **kw_predicates):
+    baked_predicates = _bake_predicates(predicates)
+    baked_keyword_predicates = _bake_kw_predicates(kw_predicates)
+
+    def test_args(args):
+        # TODO no length matching
+        return all(map(lambda p, x: p(x), baked_predicates, args))
+
+    def test_kw_args(kwargs):
+        # TODO no length matching
+        return all(map(lambda p_item: p_item[1](kwargs[p_item[0]]), baked_keyword_predicates))
+
     def decorator(fn):
         @wraps(fn)
         def decorated(*args, **kwargs):
-            return fn()
+            if test_args(args) and test_kw_args(kwargs):
+                return fn(*args, **kwargs)
+            else:
+                raise NoMatch
         return decorated
     return decorator
--- pypatterns/test/test_guard.py
+++ pypatterns/test/test_guard.py
@@ -2,6 +2,7 @@ import pytest
 from pypatterns.patterns import guard, NoMatch, Any
 
 
+@pytest.fixture
 def zero_arg_fn():
     def zero_args():
         return 'Test'
@@ -15,6 +16,7 @@ def test_return_decorator(zero_arg_fn):
     assert invoke_result == 'Test'
 
 
+@pytest.fixture
 def one_arg_fn():
     def one_arg(x):
         return x + 1
@@ -42,7 +44,7 @@ def test_literal_string_predicate():
         g(0)
     with pytest.raises(NoMatch):
         g('')
-    assert g('this is a ') == 'this is a string'
+    assert g('this is a') == 'this is a string'
 
 
 def test_literal_integer_predicate():
@@ -78,21 +80,54 @@ def test_type_predicate():
     with pytest.raises(NoMatch):
         g(3, 2)
     with pytest.raises(NoMatch):
-        g('eeeeeeeeee')
+        g('eeeeeeeeee', None)
     assert g('eeky', 3) == 'eekyeekyeeky'
 
 
 def test_has_attribute_predicate():
+    def f(x):
+        return x.count(2)
+    g = guard(Any.has('count'))(f)
+    with pytest.raises(NoMatch):
+        g({})
+    with pytest.raises(NoMatch):
+        g(1)
+    assert g((1, 2, 2, 3)) == 2
+
+
+def test_has_with_predicate():
+    X = type('X', (object, ), {'value': None})
+
+    def f(x):
+        return x.value
+    g = guard(Any.has(value=Any(int)))(f)
+    with pytest.raises(NoMatch):
+        g('123')
+    with pytest.raises(NoMatch):
+        x = X()
+        x.value = '321'
+        g(x)
+    x = X()
+    x.value = 321
+    assert g(x) == 321
+
+
+def test_with_regex_matching():
     assert 0
 
 
-def test_has_of_type():
+def test_any_of_legal_values():
     assert 0
 
 
-def test_recursive_dict_match():
+def test_with_keyword_args():
     assert 0
 
 
-def test_recursive_list_match():
+def test_check_varargs():
     assert 0
+
+
+def test_check_variable_keyword_args():
+    assert 0
+
