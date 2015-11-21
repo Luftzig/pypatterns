ff8eddf85ff14e29c4ed7b2c21811d7583ba66dd
Yoav Luft <yoav.luft@ajillionmax.com>
2015-11-21 18:17:46 +0200

    Bare necessities for uploading to pypi

--- setup.py
+++ setup.py
@@ -3,11 +3,22 @@ from setuptools import setup, find_packages
 
 setup(
     name="pypatterns",
-    version="0.1",
+    version="0.2.0",
     packages=find_packages(),
     author="Yoav Luft",
     author_email="yoav.luft@gmail.com",
     description="Functional style pattern matching for Python",
     license="MIT",
-    test_requires=['pytest']
+    url="https://github.com/Luftzig/pypatterns",
+    test_requires=['pytest'],
+    keywords=['functional', 'pattern matching'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
